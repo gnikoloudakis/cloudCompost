e71ef7b7e9316041e027fc8ca18837781bbaddb0
gnikoloudakis <hose.cuervo@gmail.com>
2017-01-12 12:13:53 +0200

    12/1/2017a

--- cloud_compost.py
+++ cloud_compost.py
@@ -216,6 +216,7 @@ def read_variables():
 
 
 def update_variables(dataDict):
+    global socketio
     socketio.emit('dashboard', dataDict)
 
     timestamp = datetime.now()
@@ -783,12 +784,14 @@ def soil_homogenization():
 
 
 def log_stuff(text):
+    global socketio
     Log(l_timestamp=datetime.now(), action=text, compost=compost_ID).save()
     socketio.emit('log_count', Log.objects.count())
     print('loooogggg')
 
 
 def error_stuff(text):
+    global socketio
     Errors(e_timestamp=datetime.now(), error=text, compost=compost_ID).save()
     socketio.emit('error_count', Errors.objects.count())
     print('errrrroorrrr')
@@ -1097,6 +1100,7 @@ def update_controls():
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
+    global socketio
     atest = request.args.get('test')
     if atest == 'sh':
         print(1)
@@ -1155,10 +1159,12 @@ def test():
 
 @socketio.on('chart_test')
 def chart_test(data):
+    global socketio
     socketio.emit('chart_return', {'name': 'yannis'})
 
 
 if __name__ == '__main__':
+    global socketio
     init_schedulers()
     init()
     setupSchedulers()
